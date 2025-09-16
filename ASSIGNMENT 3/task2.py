3f16cfefee73ccd011935e152c51d4769b307060
Kunal Somnath Gavit <kunalgavit285@outlook.com>
2025-09-16 11:22:16 +0530

    Add files via upload

---
+++ ASSIGNMENT 3/task2.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+square_root = math.sqrt(num)
+log_Value = math.log(num)
+sin_value = math.sin(num)
+print(f"Square Root: {square_root}")
+print(f"Logarithm: {log_Value}")
+print(f"Sine: {sin_value}")
